839ed2754273b739f69bf93b5a64332df69eec9e
gregers <gregers@telenordigital.com>
2018-10-17 15:15:22 +0200

    Add support for verifying and uploading multiple arduino boards

--- scripts/verify_and_upload.sh
+++ scripts/verify_and_upload.sh
@@ -2,21 +2,26 @@
 set -e
 
 usage() {
-  echo -n "usage: ${0} arduino_sketch_dir arduino_board arduino_port
+    echo -n "usage: ${0} arduino_sketch_dir arduino_board arduino_port [arduino_board arduino_port [...]]
 
 Verify and upload arduino sketch to a board"
 }
 
-if [[ "$#" -lt 3 ]]
+if (( $# < 3 || !($# % 2) ))
 then
-  echo Too few arguments
-  usage
-  exit 1
+    echo Wrong number of arguments: $#
+    usage
+    exit 1
 fi
 
 DIR=$1
-ARDUINO_BOARD=$2
-ARDUINO_PORT=$3
+shift
 
-arduino-cli compile --fqbn $ARDUINO_BOARD ~/Arduino/nbiot-e2e/device
-arduino-cli upload -p $ARDUINO_PORT --fqbn $ARDUINO_BOARD ~/Arduino/nbiot-e2e/device
+# for each arduino board and port
+while [[ $# -gt 1 ]]; do
+    ARDUINO_BOARD=$1
+    ARDUINO_PORT=$2
+    shift 2
+    arduino-cli compile --fqbn $ARDUINO_BOARD $DIR
+    arduino-cli upload -p $ARDUINO_PORT --fqbn $ARDUINO_BOARD $DIR
+done

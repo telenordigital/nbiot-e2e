a738c482b023fd37e49909282b2edc33a274f8ce
gregers <gregers@telenordigital.com>
2018-11-06 12:47:20 +0100

    Move cli arguments to a json config file

--- arduino-service/main.go
+++ arduino-service/main.go
@@ -6,7 +6,6 @@ import (
 	"path"
 	"strings"
 
-	"github.com/jessevdk/go-flags"
 	"gopkg.in/src-d/go-git.v4"
 )
 
@@ -18,50 +17,34 @@ type gitHash struct {
 func main() {
 	log.SetFlags(log.Ldate | log.Ltime)
 
-	var opts struct {
-		Boards  []string `short:"b"    long:"board-fqbn"     required:"true"    description:"Arduino FQBN"`
-		Ports   []string `short:"p"    long:"port"           required:"true"    description:"USB Port"`
-		Sketch  string   `short:"s"    long:"sketch"         required:"true"    description:"Sketch to upload when git-dir changes"`
-		LogDir  string   `short:"l"    long:"log-dir"        required:"true"    description:"Directory to store log files"`
-		GitDirs []string `short:"g"    long:"git-dir"        required:"true"    description:"Git repo folder for including latest commit hash in payload"`
-	}
-	_, err := flags.Parse(&opts)
+	config, err := readConfig()
 	if err != nil {
-		if flagsErr, ok := err.(*flags.Error); ok && flagsErr.Type == flags.ErrHelp {
-			os.Exit(0)
-		}
-		os.Exit(1)
-	}
-
-	if _, err := os.Stat(opts.Sketch); os.IsNotExist(err) {
 		log.Fatalln(err)
 	}
 
-	if stat, err := os.Stat(opts.LogDir); os.IsNotExist(err) {
+	if _, err := os.Stat(config.Sketch); os.IsNotExist(err) {
+		log.Fatalln(err)
+	}
+
+	if stat, err := os.Stat(config.LogDir); os.IsNotExist(err) {
 		log.Fatalln(err)
 	} else if !stat.IsDir() {
-		log.Fatalf("%s is not a directory", opts.LogDir)
+		log.Fatalf("%s is not a directory", config.LogDir)
 	}
 
-	if len(opts.Boards) > len(opts.Ports) {
-		log.Fatalln("Error: more boards than ports")
-	} else if len(opts.Ports) > len(opts.Boards) {
-		log.Fatalln("Error: more ports than boards")
-	}
+	for _, b := range config.Boards {
+		logName := path.Join(config.LogDir, strings.Replace(b.ArduinoFQBN, ":", "-", -1)+".log")
+		a := NewArduino(b.ArduinoFQBN, b.Port, logName)
 
-	for i, b := range opts.Boards {
-		logName := path.Join(opts.LogDir, strings.Replace(b, ":", "-", -1)+".log")
-		a := NewArduino(b, opts.Ports[i], logName)
-
-		if err := a.Compile(opts.Sketch, gitHashes(opts.GitDirs)); err != nil {
+		if err := a.Compile(config.Sketch, gitHashes(config.GitDirs)); err != nil {
 			log.Println("Error: ", err)
 		} else {
-			if err = a.Upload(opts.Sketch); err != nil {
+			if err = a.Upload(config.Sketch); err != nil {
 				log.Println("Error: ", err)
 			}
 		}
 
-		log.Printf("Starting serial monitor for %s %s\n", opts.Boards[0], opts.Ports[0])
+		log.Printf("Starting serial monitor for %s %s\n", b.ArduinoFQBN, b.Port)
 		go a.MonitorSerial()
 	}
 

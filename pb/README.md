c69be020b7324d5bf615fb343a7a5027701550e0
gregers <gregers@telenordigital.com>
2019-02-22 16:43:32 +0100

    Nuke old repo

--- pb/README.md
+++
@@ -1,6 +0,0 @@
-# Protobuf config
-
-Prerequisites:
-- protobuf 3.6+ installed
-
-Run `./protogen.sh` to build new code from the `message.proto`
--- scripts/cron_arduino_nbiot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euf -o pipefail
-(
-~/Arduino/nbiot-e2e/scripts/check_for_updates.sh ~/Arduino/libraries/ArduinoNBIoT | ts
-echo restart arduino service | ts
-sudo systemctl stop arduino | ts
-sudo systemctl start arduino | ts
-) &>> ~/log/arduino-nbiot-lib.log
